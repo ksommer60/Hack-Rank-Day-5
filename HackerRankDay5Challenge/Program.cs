4df5933c9cb8608032ea7b15bfed8feccfcc1216
ksommer60 <105603761+ksommer60@users.noreply.github.com>
2022-09-21 07:09:13 -0400

    Add files via upload

---
+++ HackerRankDay5Challenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HackerRankDay5Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            for (int i = 1; i < 11; i++)
+            {
+                int result = n * i;
+
+                Console.WriteLine(n + " x " + i + " = " + result);
+            }
+        }
+    }
+}
